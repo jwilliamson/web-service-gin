e11238e44a08189fc8c9cb334c24fd49459cfec1
James Williamson <jwilliamson@gmail.com>
2022-12-08 17:02:09 +0000

    Added a new album

--- main.go
+++ main.go
@@ -27,6 +27,7 @@ var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+	{ID: "4", Title: "All eyes on me", Artist: "Tupac", Price: 12.99},
 }
 
 // getAlbums responds with the list of all albums as JSON.
